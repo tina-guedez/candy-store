aebe4e82128013d2fad2a17c6237ba57a678bedd
Valentina Guedez <guedezvalentina@gmail.com>
2022-10-09 20:51:10 -0300

    Fixed linter warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {ChakraProvider} from "@chakra-ui/react";
-import { AppProps } from "next/app";
+import {AppProps} from "next/app";
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => {
   return (
