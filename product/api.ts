9c226f2c251a13e09926abb8c255ba9f29b533d8
Valentina Guedez <guedezvalentina@gmail.com>
2022-10-19 20:44:08 -0300

    Added product sum

--- product/api.ts
+++ product/api.ts
@@ -20,7 +20,16 @@ export default {
           new Promise<Product[]>((resolve, reject) => {
             Papa.parse(response.data, {
               header: true,
-              complete: (results) => resolve(results.data as Product[]),
+              complete: (results) => {
+                const products = results.data as Product[];
+
+                return resolve(
+                  products.map((product) => ({
+                    ...product,
+                    price: Number(product.price),
+                  })),
+                );
+              },
               error: (error) => reject(error.message),
             });
           }),
