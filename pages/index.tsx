cbbf41e6363e815856a3f3c382f35e08b3988833
Valentina Guedez <guedezvalentina@gmail.com>
2022-10-21 23:45:25 -0300

    Added stack and button styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React, {useMemo, useState} from "react";
 import {GetStaticProps} from "next";
-import {Button, Grid, Link, Stack, Text} from "@chakra-ui/react";
+import {Button, Flex, Grid, Link, Stack, Text} from "@chakra-ui/react";
 
 import {Product} from "../product/types";
 import api from "../product/api";
@@ -31,27 +31,45 @@ const IndexRoute: React.FC<Props> = ({products}) => {
   }, [cart]);
 
   return (
-    <Stack>
+    <Stack spacing={6}>
       <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
         {products.map((product) => (
-          <Stack key={product.id} backgroundColor="gray.100">
-            <Text>{product.title}</Text>
-            <Text>{parseCurrency(product.price)}</Text>
-            <Button colorScheme="primary" onClick={() => setCart((cart) => cart.concat(product))}>
+          <Stack
+            key={product.id}
+            backgroundColor="gray.100"
+            borderRadius="md"
+            padding={4}
+            spacing={3}
+          >
+            <Stack spacing={1}>
+              <Text>{product.title}</Text>
+              <Text color="green.500" fontSize="sm" fontWeight="500">
+                {parseCurrency(product.price)}
+              </Text>
+            </Stack>
+            <Button
+              colorScheme="primary"
+              size="sm"
+              variant="outline"
+              onClick={() => setCart((cart) => cart.concat(product))}
+            >
               Agregar
             </Button>
           </Stack>
         ))}
       </Grid>
       {Boolean(cart.length) && (
-        <Button
-          isExternal
-          as={Link}
-          colorScheme="whatsapp"
-          href={`https://wa.me/5491141414141?text=${encodeURIComponent(text)}`}
-        >
-          Ver carrito ({cart.length} productos)
-        </Button>
+        <Flex alignItems="center" bottom={4} justifyContent="center" position="sticky">
+          <Button
+            isExternal
+            as={Link}
+            colorScheme="whatsapp"
+            href={`https://wa.me/5491141414141?text=${encodeURIComponent(text)}`}
+            width="fit-content"
+          >
+            Ver carrito ({cart.length} productos)
+          </Button>
+        </Flex>
       )}
     </Stack>
   );
